d9500380e5242a34cea3a31bd8ecb4ce2b1fac72
good <good@DESKTOP-F2BVJ85>
2024-07-13 14:13:06 +0900

    get과 post 사이 어딘가...

--- src/main/java/com/basic/haru/login/controller/LoginController.java
+++ src/main/java/com/basic/haru/login/controller/LoginController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 // 로그인 관련 컨트롤러
 @Controller
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/login", method = RequestMethod.POST)
 public class LoginController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 	// 로그인 하기 컨트롤러
+	/*
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login() {
 		return "main/main"; // 성공하면 메인화면으로 이동 
+	}
+	*/
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login() {
+		return "redirect:/login/memberJoin"; // 성공하면 메인화면으로 이동 
 	}
 	
 	// 회원가입 페이지로 이동 컨트롤러
@@ -26,7 +33,7 @@ public class LoginController {
 	}
 	
 	// 회원가입 컨트롤러
-	@RequestMapping(value = "/memberJoin", method = RequestMethod.POST)
+	@RequestMapping(value = "/memberJoin", method = RequestMethod.GET)
 	public String memberJoin() {
 		logger.info(">>>>>>  MemberJoinMoveController");
 		return "main/main"; // 성공하면 메인화면으로 이동 
